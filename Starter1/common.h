6971af54dbb848ea5ced9dfe4e6b8c59c651f37e
mdrafiur <rafiur.rashid@mail.utoronto.ca>
2015-10-11 13:56:58 -0400

    Update common.h

--- Starter1/common.h
+++ Starter1/common.h
@@ -28,8 +28,8 @@
 #define MAX_IDENTIFIER 32
 #define MAX_TEXT       256
 #define MAX_INTEGER    32767
-#define MAX_INT        pow(2,21)
-#define MIN_INT        -MAX_INT
+#define MAX_INT        (1 << 21)
+#define MIN_INT        -(1 << 21)
 
 /********************************************************************** 
  * External declarations for variables declared in globalvars.c.

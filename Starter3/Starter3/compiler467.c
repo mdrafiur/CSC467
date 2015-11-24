237bfb7682280c642406c184837479960f9208c9
mdrafiur <rafiur.rashid@mail.utoronto.ca>
2015-11-24 18:23:20 -0500

    Update compiler467.c

--- Starter3/Starter3/compiler467.c
+++ Starter3/Starter3/compiler467.c
@@ -90,6 +90,8 @@ int main (int argc, char *argv[]) {
 /* Phase 3: Call the AST dumping routine if requested */
   if (dumpAST)
     ast_print(ast);
+    
+  add_symtable(ast);
 /* Phase 4: Add code to call the code generation routine */
 /* TODO: call your code generation routine here */
   if (errorOccurred)

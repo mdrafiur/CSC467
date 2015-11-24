0a03ea34b33b9fcf5c5fa530f1bcbfd0f48d3b85
Rashid  Md Rafiur Rashid Tajbir <rashidmd@ug61.eecg>
2015-11-24 16:05:03 -0500

    updated symbol.c

--- Starter3/Starter3/symbol.c
+++ Starter3/Starter3/symbol.c
@@ -6,59 +6,59 @@
 #include "ast.h"
 #include "symtable.h"
 
-void symtable_add(node *ast) {
+void add_symtable(node *ast) {
 
-  assert(ast);
+    assert(ast);
 
-	switch((int)ast->kind){
-		case 0:
-			break;
-			
-		case 1:
-			build_table(ast->scope.declarations);
-			build_table(ast->scope.statements);
-			break;
-			
-		case 2:
-			build_table(ast->declarations.declarations);
-			build_table(ast->declarations.declaration);
-			break;
-			
-		case 3:
-			build_table(ast->statements.statements);
-			build_table(ast->statements.statement);
-			break;
-			
-		case 4:
-		  build_table(ast->type_declaration.type);
-			build_table(ast->type_declaration.id);
-			break;
-			
-		case 5:
-		  build_table(ast->assign_declaration.type);
-			build_table(ast->assign_declaration.id);
-			build_table(ast->assign_declaration.expression);
-			break;
-			
-		case 6:
-		  build_table(ast->const_declaration.type);
-			build_table(ast->const_declaration.id);
-			build_table(ast->const_declaration.expression);
-			break;
-			
-		case 7:
-		  build_table(ast->assign_statement.variable);
-			build_table(ast->assign_statement.expression);
-			break;
-		case 9:
-			break;
-		case 10:
-			break;
-		case 11:
-			break;
-		case 12:
-			break;
-		case 13:
-			break;
-		case 14:
-			break;
+    switch((int)ast->kind){
+        case 0:
+            break;
+
+        case 1:
+            add_symtable(ast->scope.declarations);
+            add_symtable(ast->scope.statements);
+            break;
+
+        case 2:
+            add_symtable(ast->declarations.declarations);
+            add_symtable(ast->declarations.declaration);
+            break;
+
+        case 3:
+            add_symtable(ast->statements.statements);
+            add_symtable(ast->statements.statement);
+            break;
+
+        case 4:
+            add_symtable(ast->type_declaration.type);
+            add_symtable(ast->type_declaration.id);
+            break;
+
+        case 5:
+            add_symtable(ast->assign_declaration.type);
+            add_symtable(ast->assign_declaration.id);
+            add_symtable(ast->assign_declaration.expression);
+            break;
+
+        case 6:
+            add_symtable(ast->const_declaration.type);
+            add_symtable(ast->const_declaration.id);
+            add_symtable(ast->const_declaration.expression);
+            break;
+
+        case 7:
+            add_symtable(ast->assign_statement.variable);
+            add_symtable(ast->assign_statement.expression);
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        case 11:
+            break;
+        case 12:
+            break;
+        case 13:
+            break;
+        case 14:
+            break;

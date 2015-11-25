870bb5c929e7c094b38e1ae6e18e05fdf951d802
mdrafiur <rafiur.rashid@mail.utoronto.ca>
2015-11-24 19:17:18 -0500

    Update semantic.c

--- Starter3/Starter3/semantic.c
+++ Starter3/Starter3/semantic.c
@@ -1,6 +1,24 @@
-
 #include "semantic.h"
 
+int num_scope = 0;
+int right_expr, left_expr;
+
 int semantic_check( node *ast) {
+  
+    assert(ast);
+
+    switch((int)ast->kind){
+        case 0:
+            break;
+
+        case 1:
+          right_expr = semantic_check(ast->scope.declarations);
+			    left_expr = semantic_check(ast->scope.statements);
+			    
+			    if(right_expr && left_expr)
+			      return 0;
+			      
+			     
+			    
   return 0; // failed checks
 }

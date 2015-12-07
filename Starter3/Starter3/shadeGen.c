1408d44e51dc2de8cf770fb4d1c13cf88ce06e93
lianona <joy20130@gmail.com>
2015-12-06 21:23:59 -0500

    Update shadeGen.c

--- Starter3/Starter3/shadeGen.c
+++ Starter3/Starter3/shadeGen.c
@@ -1,5 +1,6 @@
 #include "shadeGen.h"
 
+
 int InIf = 0;
 int IfCompare = 0;
 //for statement & declaration
@@ -9,9 +10,15 @@ int tempregCount = 0;
 //at most 20 nested if_statement;
 int cond = 0;
 int inCond = 0;
-int scopeNum = 0;
+//int scopeNum = 0;
+FILE * fragFile;
+int Maxtemp = 0;
+int Maxcon = 0;
 
-char regComp(int index) 
+//int boots(node *ast);
+//int printToFile(node *ast);
+
+char regComp(int index)  
 {
 	switch (index) {
 	case 0:
@@ -25,6 +32,15 @@ char regComp(int index)
 	}
 }
 
+int boots(node *ast){
+	fragFile = fopen ("frag.txt","w");
+	printf("!!ARBfp1.0\n");
+	int tmp = printToFile(ast);
+	printf("END\n"); 
+	return tmp;
+}
+
+
 int printToFile(node *ast){
 
 	if(ast == NULL){
@@ -34,6 +50,7 @@ int printToFile(node *ast){
 	int kind = ast->kind;
 	int rightExpr;
 	int leftExpr;
+	int type;
 	
 	switch (kind){
 
@@ -104,13 +121,25 @@ int printToFile(node *ast){
 		if(rightExpr == -1){
 			return -1;
 		}
+		
+		leftExpr = printToFile(ast->assign_statement.variable);
+		
 		if(inCond > 0){
-			fprintf(fragFile,"CMP %s,con%d,%s,temReg%d;\n", ast->assign_statement.variable,cond, ast->assign_statement.variable, tempregCount);
+			fprintf(fragFile,"CMP ");
+			leftExpr = printToFile(ast->assign_statement.variable);
+			if(leftExpr == -1){
+				return -1;
+			}
+			fprintf(fragFile,", con%d, ", cond);
+			leftExpr = printToFile(ast->assign_statement.variable);
+			fprintf(fragFile,", temReg%d;\n", tempregCount);
 			tempregCount--;
 
 		}else{
 		//fprintf(fragFile,"TEMP %s;\n", ast->assign_statement.variable);
-			fprintf(fragFile, "MOV %s, temReg%d;\n", ast->assign_statement.variable, tempregCount);
+			fprintf(fragFile, "MOV ");
+			leftExpr = printToFile(ast->assign_statement.variable);
+			fprintf(fragFile, "temReg%d;\n", tempregCount);
 			tempregCount--;
 		}
 		return 0;
@@ -121,20 +150,23 @@ int printToFile(node *ast){
 		
 		
 		cond++;
-		fprintf(fragFile,"TEMP  con%d;\n", cond);
-		if(inCond > 0){
-			fprintf(fragFile,"MOV  con%d, -1;\n", cond);
-		}else{
-	
+		if(cond > Maxcon){
+			Maxcon++;
+			fprintf(fragFile,"TEMP  con%d;\n", cond);
+		}
+		//if(inCond > 0){
+			//fprintf(fragFile,"MOV  con%d, -1;\n", cond);
+			
+		//}else{
 			rightExpr = printToFile(ast->if_statement.condition);
 			if(rightExpr == -1){
 				return -1;
 			}
-		}
+		//}
+
 		inCond++;
 		fprintf(fragFile,"MOV  con%d, temReg%d;\n", cond, tempregCount);
 		tempregCount--;
-		
 		
 		fprintf(fragFile,"#then\n");
 		leftExpr  = printToFile(ast->if_statement.statement);
@@ -151,14 +183,18 @@ int printToFile(node *ast){
 	case NIF_ELSE_STATEMENT:
 		
 		cond++;
-		//fprintf(fragFile,"TEMP reg%d;\n", registerCount);
-		if(inCond > 0){
-			fprintf(fragFile,"MOV  con%d, -1;\n", cond);
+		//if(inCond > 0){
+			//fprintf(fragFile,"MOV  con%d, -1;\n", cond);
+		//}
+		if(cond > Maxcon){
+			Maxcon++;
+			fprintf(fragFile,"TEMP  con%d;\n", cond);
 		}
 		rightExpr = printToFile(ast->if_statement.condition);
 		if(rightExpr == -1){
 			return -1;
 		}
+		
 		inCond++;
 		fprintf(fragFile,"MOV  con%d, temReg%d;\n", cond, tempregCount);
 		tempregCount--;
@@ -169,12 +205,13 @@ int printToFile(node *ast){
 			return -1;
 		}
 		fprintf(fragFile,"#else\n");
-		if(inCond <= 1){
-			fprintf(fragFile,"MUL con%d, MUL con%d,-1;\n", inCond, inCond);
-		}
+		//if(inCond <= 1){
+			//fprintf(fragFile,"MUL con%d, MUL con%d,-1;\n", inCond, inCond);
+		//}
 		
 		leftExpr  = printToFile(ast->if_else_statement.else_statement);
 		inCond--;
+		cond--;
 		if(leftExpr == -1){
 			return -1;
 		}else{
@@ -189,8 +226,7 @@ int printToFile(node *ast){
 			if(rightExpr == -1){
 				return -1;
 			}
-			
-			fprintf("MUL temReg%d, temReg%d, -1;\n",tempregCount, tempregCount);
+			fprintf(fragFile,"MUL temReg%d, temReg%d, -1;\n",tempregCount, tempregCount);
 			return 0;
 			break;
 		/*11 This part is not complete!!!*/
@@ -199,51 +235,53 @@ int printToFile(node *ast){
 			if(rightExpr == -1){
 				return -1;
 			}
-			leftExpr = printToFile(ast->unary_expr.left);
+			leftExpr = printToFile(ast->binary_expr.left);
 			if(leftExpr == -1){
 				return -1;
 			}
 			if(ast->binary_expr.op == AND_OPS){
-				fprintf("MIN temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+				fprintf(fragFile,"MIN temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
 				tempregCount --;
 			}else if(ast->binary_expr.op == OR_OPS){
-				fprintf("MAX temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+				fprintf(fragFile,"MAX temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
 				tempregCount --;
 			}else if(ast->binary_expr.op == EQ_OPS){
-				fprintf("SUB temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
-				fprintf("SGE temReg%d, temReg%d, 0;\n", tempregCount, tempregCount - 1);
-				fprintf("SLT temReg%d, temReg%d, 0;\n", tempregCount - 1, tempregCount - 1);
+				fprintf(fragFile,"SUB temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+				fprintf(fragFile,"SGE temReg%d, temReg%d, 0;\n", tempregCount, tempregCount - 1);
+				fprintf(fragFile,"SLT temReg%d, temReg%d, 0;\n", tempregCount - 1, tempregCount - 1);
 				tempregCount --;
-				fprintf("SUB temReg%d, temReg%d, 1;\n", tempregCount, tempregCount);
-				fprintf("CMP temReg%d, temReg%d, -1, 1;\n", tempregCount, tempregCount);
+				fprintf(fragFile,"SUB temReg%d, temReg%d, 1;\n", tempregCount, tempregCount);
+				fprintf(fragFile,"CMP temReg%d, temReg%d, -1, 1;\n", tempregCount, tempregCount);
 			}else if(ast->binary_expr.op == NEQ_OPS){
-				fprintf("SUB temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
-                fprintf("SGE temReg%d, temReg%d, 1;\n", tempregCount, tempregCount - 1);
-                fprintf("SLT temReg%d, temReg%d, 1;\n", tempregCount - 1, tempregCount - 1);
+				fprintf(fragFile,"SUB temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+                fprintf(fragFile,"SGE temReg%d, temReg%d, 0;\n", tempregCount, tempregCount - 1);
+                fprintf(fragFile,"SLT temReg%d, temReg%d, 0;\n", tempregCount - 1, tempregCount - 1);
                 tempregCount--;
-                fprintf("SUB temReg%d, temReg%d, 0;\n", tempregCount, tempregCount);
-                fprintf("CMP temReg%d, temReg%d, 1, -1;\n", tempregCount, tempregCount);
+                fprintf(fragFile,"SUB temReg%d, temReg%d, 1;\n", tempregCount, tempregCount);
+                fprintf(fragFile,"CMP temReg%d, temReg%d, 1, -1;\n", tempregCount, tempregCount);
 			}else if(ast->binary_expr.op == GEQ_OPS){
-                fprintf("SGE temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+                fprintf(fragFile,"SGE temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
                 tempregCount--;
-                fprintf("SUB temReg%d, temReg%d, 1;\n", tempregCount, tempregCount);
-                fprintf("CMP temReg%d, temReg%d, -1, 1;\n", tempregCount, tempregCount);
+                fprintf(fragFile,"SUB temReg%d, temReg%d, 1;\n", tempregCount, tempregCount);
+                fprintf(fragFile,"CMP temReg%d, temReg%d, -1, 1;\n", tempregCount, tempregCount);
             }else if(ast->binary_expr.op == LEQ_OPS){
-                fprintf("SGE temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+                fprintf(fragFile,"SGE temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
                 tempregCount--;
-                fprintf("SUB temReg%d, temReg%d, 1;\n", tempregCount, tempregCount);
-                fprintf("CMP temReg%d, temReg%d, 1, -1;\n", tempregCount, tempregCount);
+                fprintf(fragFile,"SUB temReg%d, temReg%d, 1;\n", tempregCount, tempregCount);
+                fprintf(fragFile,"CMP temReg%d, temReg%d, -1, 1;\n", tempregCount, tempregCount);
             }else if(ast->binary_expr.op == PLUS_OPS){
-                fprintf("ADD temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+                fprintf(fragFile,"ADD temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+				tempregCount--;
             }else if(ast->binary_expr.op == MINUS_OPS){
-                fprintf("SUB temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+                fprintf(fragFile,"SUB temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+				tempregCount--;
             }else if(ast->binary_expr.op == TIMES_OPS){
-                fprintf("MUL temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+                fprintf(fragFile,"MUL temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+				tempregCount--;
             }else if(ast->binary_expr.op == LESS_OPS){
-                fprintf("SGE temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
+                fprintf(fragFile,"SUB temReg%d, temReg%d, temReg%d;\n", tempregCount - 1, tempregCount, tempregCount - 1);
                 tempregCount--;
-                fprintf("SUB temReg%d, temReg%d, 1;\n", tempregCount, tempregCount);
-                fprintf("CMP temReg%d, temReg%d, 1, -1;\n", tempregCount, tempregCount);
+                fprintf(fragFile,"CMP temReg%d, temReg%d, 1, -1;\n", tempregCount - 1, tempregCount -1);
             }
             break;
 
@@ -253,8 +291,12 @@ int printToFile(node *ast){
 			break;
 		
 		case NFUNC_EXPR:
+			
 			tempregCount++;
-			fprintf(fragFile,"TEMP temReg%d;\n", tempregCount);
+			if(tempregCount > Maxtemp){
+				fprintf(fragFile,"TEMP temReg%d;\n", tempregCount);
+				Maxtemp++;
+			}
 
 			if(ast->func_expr.func == 0){
 				fprintf(fragFile,"DP3 temReg%d, ", tempregCount);
@@ -270,8 +312,8 @@ int printToFile(node *ast){
 			break;
 			
 		case NTYPE_EXPR:
-			leftExpr = printToFile(ast->type_expr.type);
-			fprintf(fragFile, "TEMP temReg%d = {", tempregCount);
+			//leftExpr = printToFile(ast->type_expr.type);
+			fprintf(fragFile, "{");
 			rightExpr = printToFile(ast->type_expr.arguments_opt);
 			fprintf(fragFile, "}");
 			return 0;
@@ -283,18 +325,31 @@ int printToFile(node *ast){
 			break;
 			
 		case NINT_EXPR:
+			tempregCount++;
+			if(tempregCount > Maxtemp){
+				fprintf(fragFile,"TEMP temReg%d;\n", tempregCount);
+				Maxtemp++;
+			}
 			fprintf(fragFile, "MOV temReg%d, %d.0;\n", tempregCount, ast->int_expr.number);
 			return 0;
 			break;
 			
 		case NFLOAT_EXPR:
+			tempregCount++;
+			if(tempregCount > Maxtemp){
+				fprintf(fragFile,"TEMP temReg%d;\n", tempregCount);
+				Maxtemp++;
+			}
 			fprintf(fragFile, "MOV temReg%d, %f;\n", tempregCount, ast->float_expr.number);
 			return 0;
 			break;
 			
 		case NBOOL_EXPR:
 			tempregCount++;
-			fprintf(fragFile,"TEMP temReg%d;\n", tempregCount);
+			if(tempregCount > Maxtemp){
+				fprintf(fragFile,"TEMP temReg%d;\n", tempregCount);
+				Maxtemp++;
+			}
 			if(ast->bool_expr.boolean == 0){
 				fprintf(fragFile,"MOV  temReg%d, 1 ;\n", tempregCount);
 			}else if(ast->bool_expr.boolean == 1){
@@ -306,126 +361,126 @@ int printToFile(node *ast){
 			break;
 			
 		case NID_VARIABLE:
-			char *id = ast->id_variable.id;
+			//char *name = ast->id_variable.id;
 		
-			if (strcmp(id, "gl_FragColor") == 0) {
+			if (strcmp(ast->id_variable.id, "gl_FragColor") == 0) {
 				fprintf(fragFile, "result.color");
 				return 0;
 			} 
-			else if (strcmp(id, "gl_FragDepth") == 0) {
+			else if (strcmp(ast->id_variable.id, "gl_FragDepth") == 0) {
 				fprintf(fragFile, "result.depth");
 				return 0;
 			} 
-			else if (strcmp(id, "gl_FragCoord") == 0) {
+			else if (strcmp(ast->id_variable.id, "gl_FragCoord") == 0) {
 				fprintf(fragFile, "fragment.position");
 				return 0;
 			} 
-			else if (strcmp(id, "gl_TexCoord") == 0) {
+			else if (strcmp(ast->id_variable.id, "gl_TexCoord") == 0) {
 				fprintf(fragFile, "fragment.texcoord");
 				return 0;
 			} 
-			else if (strcmp(id, "gl_Color") == 0) {
+			else if (strcmp(ast->id_variable.id, "gl_Color") == 0) {
 				fprintf(fragFile, "fragment.color");
 				return 0;
 			} 
-			else if (strcmp(id, "gl_Secondary") == 0) {
+			else if (strcmp(ast->id_variable.id, "gl_Secondary") == 0) {
 				fprintf(fragFile, "fragment.color.secondary");
 				return 0;
 			} 
-			else if (strcmp(id, "gl_FogFragCoord") == 0) {
+			else if (strcmp(ast->id_variable.id, "gl_FogFragCoord") == 0) {
 				fprintf(fragFile, "fragment.fogcoord");
 				return 0;
 			} 
-			else if (strcmp(id, "gl_Light_Half") == 0) {
+			else if (strcmp(ast->id_variable.id, "gl_Light_Half") == 0) {
 				fprintf(fragFile, "state.light[0].half");
 				return 0;
 			} 
-			else if (strcmp(id, "gl_Light_Ambient") == 0) {
+			else if (strcmp(ast->id_variable.id, "gl_Light_Ambient") == 0) {
 				fprintf(fragFile, "state.lightmodel.ambient");
 				return 0;
 			} 
-			else if (strcmp(id, "gl_Material_Shininess") == 0) {
+			else if (strcmp(ast->id_variable.id, "gl_Material_Shininess") == 0) {
 				fprintf(fragFile, "state.material.shininess");
 				return 0;
 			} 
-			else if (strcmp(id, "env1") == 0) {
+			else if (strcmp(ast->id_variable.id, "env1") == 0) {
 				fprintf(fragFile, "program.env[1]");
 				return 0;
 			} 
-			else if (strcmp(id, "env2") == 0) {
+			else if (strcmp(ast->id_variable.id, "env2") == 0) {
 				fprintf(fragFile, "program.env[2]");
 				return 0;
 			} 
-			else if (strcmp(id, "env3") == 0) {
+			else if (strcmp(ast->id_variable.id, "env3") == 0) {
 				fprintf(fragFile, "program.env[3]");
 				return 0;
 			} 
 			else {
-				fprintf(fragFile, "%s", id);
+				fprintf(fragFile, "%s", ast->id_variable.id);
 				return 0;
 			}
 			return 0;
 			break;
 			
 		case NARRAY_VARIABLE:
-			char *id = ast->array_variable.id;
-			int type = get_data_type(sym_table, id);
-			int index = ast->array_variable.index;
+			//char *id = ast->array_variable.id;
+			type = get_data_type(sym_table, ast->array_variable.id);
+			//int index = ast->array_variable.index;
 
-			if (strcmp(id, "gl_FragColor") == 0) {
-				fprintf(fragFile, "result.color.%c", regComp(index));
+			if (strcmp(ast->array_variable.id, "gl_FragColor") == 0) {
+				fprintf(fragFile, "result.color.%c", regComp(ast->array_variable.index));
 				return 0;
 		
-			} else if (strcmp(id, "gl_FragDepth") == 0) {
-				fprintf(fragFile, "result.depth.%c", regComp(index));
+			} else if (strcmp(ast->array_variable.id, "gl_FragDepth") == 0) {
+				fprintf(fragFile, "result.depth.%c", regComp(ast->array_variable.index));
 				return 0;
 		
-			} else if (strcmp(id, "gl_FragCoord") == 0) {
-				fprintf(fragFile, "fragment.position.%c", regComp(index));
+			} else if (strcmp(ast->array_variable.id, "gl_FragCoord") == 0) {
+				fprintf(fragFile, "fragment.position.%c", regComp(ast->array_variable.index));
 				return 0;
 		
-			} else if (strcmp(id, "gl_TexCoord") == 0) {
-				fprintf(fragFile, "fragment.texcoord.%c", regComp(index));
+			} else if (strcmp(ast->array_variable.id, "gl_TexCoord") == 0) {
+				fprintf(fragFile, "fragment.texcoord.%c", regComp(ast->array_variable.index));
 				return 0;
 		
-			} else if (strcmp(id, "gl_Color") == 0) {
-				fprintf(fragFile, "fragment.color.%c", regComp(index));
+			} else if (strcmp(ast->array_variable.id, "gl_Color") == 0) {
+				fprintf(fragFile, "fragment.color.%c", regComp(ast->array_variable.index));
 				return 0;
 		
-			} else if (strcmp(id, "gl_Secondary") == 0) {
-				fprintf(fragFile, "fragment.color.secondary.%c", regComp(index));
+			} else if (strcmp(ast->array_variable.id, "gl_Secondary") == 0) {
+				fprintf(fragFile, "fragment.color.secondary.%c", regComp(ast->array_variable.index));
 				return 0;
 		
-			} else if (strcmp(id, "gl_FogFragCoord") == 0) {
-				fprintf(fragFile, "fragment.fogcoord.%c", regComp(index));
+			} else if (strcmp(ast->array_variable.id, "gl_FogFragCoord") == 0) {
+				fprintf(fragFile, "fragment.fogcoord.%c", regComp(ast->array_variable.index));
 				return 0;
 		
-			} else if (strcmp(id, "gl_Light_Half") == 0) {
-				fprintf(fragFile, "state.light[0].half.%c", regComp(index));
+			} else if (strcmp(ast->array_variable.id, "gl_Light_Half") == 0) {
+				fprintf(fragFile, "state.light[0].half.%c", regComp(ast->array_variable.index));
 				return 0;
 		
-			} else if (strcmp(id, "gl_Light_Ambient") == 0) {
-				fprintf(fragFile, "state.lightmodel.ambient.%c", regComp(index));
+			} else if (strcmp(ast->array_variable.id, "gl_Light_Ambient") == 0) {
+				fprintf(fragFile, "state.lightmodel.ambient.%c", regComp(ast->array_variable.index));
 				return 0;
 		
-			} else if (strcmp(id, "gl_Material_Shininess") == 0) {
-				fprintf(fragFile, "state.material.shininess.%c", regComp(index));
+			} else if (strcmp(ast->array_variable.id, "gl_Material_Shininess") == 0) {
+				fprintf(fragFile, "state.material.shininess.%c", regComp(ast->array_variable.index));
 				return 0;
 		
-			} else if (strcmp(id, "env1") == 0) {
-				fprintf(fragFile, "program.env[1].%c", regComp(index));
+			} else if (strcmp(ast->array_variable.id, "env1") == 0) {
+				fprintf(fragFile, "program.env[1].%c", regComp(ast->array_variable.index));
 				return 0;
 		
-			} else if (strcmp(id, "env2") == 0) {
-				fprintf(fragFile, "program.env[2].%c", regComp(index));
+			} else if (strcmp(ast->array_variable.id, "env2") == 0) {
+				fprintf(fragFile, "program.env[2].%c", regComp(ast->array_variable.index));
 				return 0;
 		
-			} else if (strcmp(id, "env3") == 0) {
-				fprintf(fragFile, "program.env[3].%c", regComp(index));
+			} else if (strcmp(ast->array_variable.id, "env3") == 0) {
+				fprintf(fragFile, "program.env[3].%c", regComp(ast->array_variable.index));
 				return 0;
 		
 			} else {
-				fprintf(fragFile, "%s.%c", id, regComp(index));
+				fprintf(fragFile, "%s.%c", ast->array_variable.id, regComp(ast->array_variable.index));
 				return 0;
 			}
 			break;
@@ -454,9 +509,9 @@ int printToFile(node *ast){
 			break;
 			
 		case NPROG_SCOPE:
-			scopeNum++;
+			//scopeNum++;
 			rightExpr = printToFile(ast->prog_scope.scope);
-			scopeNum--;
+			//scopeNum--;
 			return rightExpr;
 			break;
 	}
